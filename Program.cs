7e6378ad4efa1e70d0ec63c9145860bc269e9cce
Stephen Halter <halter73@gmail.com>
2018-02-09 16:26:11 -0800

    Introduce build error displayed by msbuild

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace signalr_verification
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            foo
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
